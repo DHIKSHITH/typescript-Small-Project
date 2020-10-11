7c5d1020a698abc17ab74ca8d1bc8d9b4ce4b835
Dhikshith Suresh <dikshithakon@gmail.com>
2020-10-11 14:38:07 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log(user.location);
+console.log(user.name);
+
+console.log(company.location);
+console.log(company.companyName);
+console.log(company.catchPhrase);
